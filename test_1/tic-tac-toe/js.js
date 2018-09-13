18bb50f17d4c93d11aeb29759bba595689aa1501
nadiiabilockon <40208527+nadiiabilockon@users.noreply.github.com>
2018-09-13 13:22:35 +0300

    Update js.js

--- test_1/tic-tac-toe/js.js
+++ test_1/tic-tac-toe/js.js
@@ -10,38 +10,54 @@ window.onload = function () {
     game.onclick = function (event) {
         console.log(event);
         if (event.target.className == 'block') {
-            if (hod % 2 == 0) {
-                event.target.innerHTML = "X";
-            }
-            else {
-                event.target.innerHTML = "0";
-            }
-            hod++;
+                if (hod % 2 == 0 && event.target.innerText == "") {
+                    event.target.innerHTML = "X";
+                }
+                else{
+                    onclick();
+                }
+            hod++; 
+            aiClick();
             checkWinner();
         }
+    }
+    function aiClick() {
+        if (hod == 9) {
+            return;
+        }
+        var allblock = document.getElementsByClassName('block');
+        var id = Math.floor(Math.random() * 9);
+        if (allblock[id].innerText == "") {
+            allblock[id].innerHTML = "0";
+        }
+        else {
+            aiClick();
+        }
+        hod++;
+
     }
     function checkWinner() {
         var allblock = document.getElementsByClassName('block');
 
         // Check if win X
-        if (allblock[0].innerHTML == "X" && allblock[1].innerHTML == "X" && allblock[2].innerHTML == "X")  alert("Win X");
-        if (allblock[3].innerHTML == "X" && allblock[4].innerHTML == "X" && allblock[5].innerHTML == "X")  alert("Win X");
-        if (allblock[6].innerHTML == "X" && allblock[7].innerHTML == "X" && allblock[8].innerHTML == "X")  alert("Win X");
-        if (allblock[0].innerHTML == "X" && allblock[3].innerHTML == "X" && allblock[6].innerHTML == "X")  alert("Win X");
-        if (allblock[1].innerHTML == "X" && allblock[4].innerHTML == "X" && allblock[7].innerHTML == "X")  alert("Win X");
-        if (allblock[2].innerHTML == "X" && allblock[5].innerHTML == "X" && allblock[8].innerHTML == "X")  alert("Win X");
-        if (allblock[0].innerHTML == "X" && allblock[4].innerHTML == "X" && allblock[8].innerHTML == "X")  alert("Win X");
-        if (allblock[2].innerHTML == "X" && allblock[4].innerHTML == "X" && allblock[6].innerHTML == "X")  alert("Win X");
+        if (allblock[0].innerHTML == "X" && allblock[1].innerHTML == "X" && allblock[2].innerHTML == "X") alert("Win X");
+        if (allblock[3].innerHTML == "X" && allblock[4].innerHTML == "X" && allblock[5].innerHTML == "X") alert("Win X");
+        if (allblock[6].innerHTML == "X" && allblock[7].innerHTML == "X" && allblock[8].innerHTML == "X") alert("Win X");
+        if (allblock[0].innerHTML == "X" && allblock[3].innerHTML == "X" && allblock[6].innerHTML == "X") alert("Win X");
+        if (allblock[1].innerHTML == "X" && allblock[4].innerHTML == "X" && allblock[7].innerHTML == "X") alert("Win X");
+        if (allblock[2].innerHTML == "X" && allblock[5].innerHTML == "X" && allblock[8].innerHTML == "X") alert("Win X");
+        if (allblock[0].innerHTML == "X" && allblock[4].innerHTML == "X" && allblock[8].innerHTML == "X") alert("Win X");
+        if (allblock[2].innerHTML == "X" && allblock[4].innerHTML == "X" && allblock[6].innerHTML == "X") alert("Win X");
 
         // Check if win 0
-        if (allblock[0].innerHTML == "0" && allblock[1].innerHTML == "0" && allblock[2].innerHTML == "0")  alert("Win 0");
-        if (allblock[3].innerHTML == "0" && allblock[4].innerHTML == "0" && allblock[5].innerHTML == "0")  alert("Win 0");
-        if (allblock[6].innerHTML == "0" && allblock[7].innerHTML == "0" && allblock[8].innerHTML == "0")  alert("Win 0");
-        if (allblock[0].innerHTML == "0" && allblock[3].innerHTML == "0" && allblock[6].innerHTML == "0")  alert("Win 0");
-        if (allblock[1].innerHTML == "0" && allblock[4].innerHTML == "0" && allblock[7].innerHTML == "0")  alert("Win 0");
-        if (allblock[2].innerHTML == "0" && allblock[5].innerHTML == "0" && allblock[8].innerHTML == "0")  alert("Win 0");
-        if (allblock[0].innerHTML == "0" && allblock[4].innerHTML == "0" && allblock[8].innerHTML == "0")  alert("Win 0");
-        if (allblock[2].innerHTML == "0" && allblock[4].innerHTML == "0" && allblock[6].innerHTML == "0")  alert("Win 0");
+        if (allblock[0].innerHTML == "0" && allblock[1].innerHTML == "0" && allblock[2].innerHTML == "0") alert("Win 0");
+        if (allblock[3].innerHTML == "0" && allblock[4].innerHTML == "0" && allblock[5].innerHTML == "0") alert("Win 0");
+        if (allblock[6].innerHTML == "0" && allblock[7].innerHTML == "0" && allblock[8].innerHTML == "0") alert("Win 0");
+        if (allblock[0].innerHTML == "0" && allblock[3].innerHTML == "0" && allblock[6].innerHTML == "0") alert("Win 0");
+        if (allblock[1].innerHTML == "0" && allblock[4].innerHTML == "0" && allblock[7].innerHTML == "0") alert("Win 0");
+        if (allblock[2].innerHTML == "0" && allblock[5].innerHTML == "0" && allblock[8].innerHTML == "0") alert("Win 0");
+        if (allblock[0].innerHTML == "0" && allblock[4].innerHTML == "0" && allblock[8].innerHTML == "0") alert("Win 0");
+        if (allblock[2].innerHTML == "0" && allblock[4].innerHTML == "0" && allblock[6].innerHTML == "0") alert("Win 0");
     }
-    
+
 }
