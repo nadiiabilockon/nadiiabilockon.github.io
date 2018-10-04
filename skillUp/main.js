b0bda9919b69356489d4651ae5c33b666139d808
nadiiabilockon <40208527+nadiiabilockon@users.noreply.github.com>
2018-10-05 00:33:24 +0300

    Add files via upload

---
+++ skillUp/main.js
@@ -0,0 +1,15 @@
+//first task
+var number = +prompt("Please enter the number from 25 to 99","")
+if(number >= 25 || number >= 99){
+    console.log("You enter number"+ " " + number);
+}
+else{
+    console.log("You enter not correct number");
+}
+
+//second task
+var a = +prompt("Please enter the number a","");
+var b = +prompt("Please enter the number b","");
+var result = (a === b) ? console.log("a and b are equal") : (a > b) ? console.log("a is greater") : console.log("b is greater");
+
+//third task in index.html
