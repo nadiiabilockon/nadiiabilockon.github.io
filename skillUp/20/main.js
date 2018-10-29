3e5fd29c6b57cd2453f183b9907e0f0c295c0bd8
nadiiabilockon <40208527+nadiiabilockon@users.noreply.github.com>
2018-10-29 15:18:24 +0200

    Add files via upload

--- skillUp/20/main.js
+++ skillUp/20/main.js
@@ -1,5 +1,7 @@
-let myNodeList = document.querySelectorAll('.line');
-let result = document.querySelector('.total'); 
+// table №1
+
+const myNodeList = document.querySelectorAll('.line');
+const result = document.querySelector('.total'); 
 
 NodeList.prototype.customEventListener = function (event_name, callback, useCapture){
     for (let i = 0; i < this.length; i++){
@@ -20,24 +22,17 @@ myNodeList.customEventListener('input', function(e) {
         let amount =  myNodeList[i].querySelector('.amount').value;
         sum +=  parseFloat(price * amount);
     }
-    result.innerHTML = ""+ sum.toFixed(2);
+    result.innerHTML = " "+ sum.toFixed(2);
 });
 
+// table №2
 
-let new_result = document.querySelector('.total_new'); 
-var maxField = 10;
-var y = 1;
-var add_btn = document.querySelector(".add");
-var templ = document.querySelector(".template .line_new");
-var edite_table = document.querySelector(".edite_table");
-
-add_btn.addEventListener('click', function() {
-    if(y < maxField){ 
-        y++;
-        var copy = templ.cloneNode(true);
-        edite_table.appendChild(copy);
-    }
-});
+const edite_table = document.querySelector(".edite_table");
+const templ = document.querySelector(".template .line_new");
+const new_result = document.querySelector('.total_new'); 
+const add_btn = document.querySelector(".add");
+let maxField = 10;
+let y = 1;
 
 edite_table.addEventListener('input', function(e) {
     if(!(e.target.classList.contains("amount") || e.target.classList.contains("price"))) {
@@ -46,13 +41,21 @@ edite_table.addEventListener('input', function(e) {
     calculateTotal();
 });
 
+add_btn.addEventListener('click', function() {
+    if(y < maxField){ 
+        y++;
+        let copy = templ.cloneNode(true);
+        edite_table.appendChild(copy);
+    }
+});
+
 edite_table.addEventListener('click', function(e) {
     console.dir(e.target);
     if(!e.target.classList.contains("remove")){
         return;
     }
     if(y != 1){ 
-        edite_table.removeChild(e.target.parentElement.parentElement);
+        edite_table.removeChild(e.target.closest('tr'));
         y--;
     }else{
         alert("You remove all")
